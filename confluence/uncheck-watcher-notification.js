7cf5f72b781bfa7450565be372b18bd11b9cc673
Patrice Matzke <p.matzke@avm.de>
2020-01-17 13:32:09 +0100

    implemented timeout

--- confluence/uncheck-watcher-notification.js
+++ confluence/uncheck-watcher-notification.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name       [Confluence] uncheck watcher notification
 // @include    https://wiki.avm.de/pages*
-// @version    1.0
+// @version    1.0.1
 // @author     https://github.com/VlkaFenryka
 // ==/UserScript==
 
 let selector = "#notifyWatchers"
 
-if (document.querySelector(selector).checked) {
-    document.querySelector(selector).checked = false
-}
+setTimeout(() => {
+    if (document.querySelector(selector).checked) {
+        document.querySelector(selector).checked = false
+    }
+}, 1000);
+
