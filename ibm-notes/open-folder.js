444cb8a244d2287bb4d2a6190e1b5e7fa2ee63fd
Patrice Matzke <p.matzke@avm.de>
2020-01-28 09:31:18 +0100

    removed eventlistener load

--- ibm-notes/open-folder.js
+++ ibm-notes/open-folder.js
@@ -1,36 +1,33 @@
 // ==UserScript==
 // @name         [IBM notes] folder opened
-// @version      1.0.0
+// @version      1.0.1
 // @author       https://github.com/VlkaFenryka
 // @include      https://mail-notes.avm.de/mail/pmatzke.nsf
 // ==/UserScript==
 
 (function() {
   "use strict";
-
-  window.addEventListener("load", event => {
-    setTimeout(function() {
-      // open main folder
-      document
-        .querySelector("#s_MainFrame")
-        .contentWindow.document.querySelector(
-          "#e-mailoutline-row-Ordner12-elem-OUTLINEELEM40"
-        )
-        .click();
-      // open Ticket folder
-      document
-        .querySelector("#s_MainFrame")
-        .contentWindow.document.querySelector(
-          "[data-in-outlineelem-id='OUTLINEELEM71']"
-        )
-        .click();
-      // open day at a glance panel
-      document
-        .querySelector("#s_MainFrame")
-        .contentWindow.document.querySelector(
-          "#e-actions-mailview-inbox-show-text"
-        )
-        .click();
-    }, 1000);
-  });
+  setTimeout(function() {
+    // open main folder
+    document
+      .querySelector("#s_MainFrame")
+      .contentWindow.document.querySelector(
+        "#e-mailoutline-row-Ordner12-elem-OUTLINEELEM40"
+      )
+      .click();
+    // open Ticket folder
+    document
+      .querySelector("#s_MainFrame")
+      .contentWindow.document.querySelector(
+        "[data-in-outlineelem-id='OUTLINEELEM71']"
+      )
+      .click();
+    // open day at a glance panel
+    document
+      .querySelector("#s_MainFrame")
+      .contentWindow.document.querySelector(
+        "#e-actions-mailview-inbox-show-text"
+      )
+      .click();
+  }, 2000);
 })();
