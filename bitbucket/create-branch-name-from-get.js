349811880e8482a3be4dfb61423e27d01b6243e7
Patrice Matzke <p.matzke@avm.de>
2020-02-12 12:20:07 +0100

    switch based on repo GET value

--- bitbucket/create-branch-name-from-get.js
+++ bitbucket/create-branch-name-from-get.js
@@ -1,41 +1,58 @@
 // ==UserScript==
 // @name       [Bitbucket] create branch name from GET
 // @include    https://bitbucket.avm.de/plugins/servlet/create-branch*
-// @version    1.0.1
+// @version    1.0.2
 // @author     https://github.com/VlkaFenryka
 // ==/UserScript==
 
 let selectors = {
-  buttonBranchFrom: "#branch-from-selector",
-  buttonRepository: "#repository-selector",
-  createBranch: "#create-branch-submit",
-  inputBranchName: "#branch-name",
-  optionRespository:
-    "#inline-dialog-repository-selector-dialog [title='FRITZ!OS / appls.html']",
-  optionBranch:
-    "#branch-from-selector-dialog [data-id='refs/heads/shared/HtmlDev/avm5next']"
+    branchOverviewSearchInput: "#paged-table-input-for-branch-list",
+    buttonBranchFrom: "#branch-from-selector",
+    buttonBranchOverview: "#repository-nav-branches",
+    buttonRepository: "#repository-selector",
+    createBranch: "#create-branch-submit",
+    inputBranchName: "#branch-name",
+    optionBranch: "#branch-from-selector-dialog",
+    optionBranchNodeHtml: "[data-id='refs/heads/shared/HtmlDev/avm5next']",
+    optionBranchNodeModules: "[data-id='refs/heads/stable/master-2']",
+    optionRespository: "#inline-dialog-repository-selector-dialog",
+    optionRespositoryHtml: "[title='FRITZ!OS / appls.html']",
+    optionRespositoryNodeModules: "[title='FRITZ!OS / appls.html.webpack']"
 };
-const refLength = 80
-
-document.querySelector(selectors.buttonRepository).click();
-document.querySelector(selectors.optionRespository).click();
-document.querySelector(selectors.buttonBranchFrom).click();
-setTimeout(() => {
-  document.querySelector(selectors.optionBranch).click();
-}, 100);
+const refLength = 80;
+const id = "pma";
 
 if (window.location.search.length > 0) {
-  const url = new URL(window.location.href);
-  const issueID = url.searchParams.get("issueid");
-  const title = url.searchParams.get("title");
-  let branchName = `p/pmatzke/JZ-${issueID}-${title}`
-  document.querySelector(
-    selectors.inputBranchName
-  ).value = branchName.substring(0, refLength);
-  setTimeout(() => {
-    document.querySelector(selectors.createBranch).click();
-  }, 100);
-  setTimeout(() => {
-    close();
-  }, 1000);
+    const url = new URL(window.location.href);
+    const issueID = url.searchParams.get("issueid");
+    const title = url.searchParams.get("title");
+    const repo = url.searchParams.get("repo");
+    let branchName = `p/pmatzke/JZ-${issueID}-${title}`;
+    document.querySelector(selectors.inputBranchName).value = branchName.substring(0, refLength);
+    let repoSelector = "";
+    let branchSelector = "";
+    switch (repo) {
+        case "html":
+            repoSelector = selectors.optionRespositoryHtml;
+            branchSelector = selectors.optionBranchNodeHtml;
+            break;
+        case "node_modules":
+            repoSelector = selectors.optionRespositoryNodeModules;
+            branchSelector = selectors.optionBranchNodeModules;
+            break;
+    }
+    document.querySelector(selectors.buttonRepository).click();
+    document.querySelector(`${selectors.optionRespository} ${repoSelector}`).click();
+    setTimeout(() => {
+        document.querySelector(selectors.buttonBranchFrom).click();
+    }, 100);
+    setTimeout(() => {
+        document.querySelector(`${selectors.optionBranch} ${branchSelector}`).click();
+    }, 200);
+    setTimeout(() => {
+        document.querySelector(selectors.createBranch).click();
+    }, 300);
+    setTimeout(() => {
+        document.querySelector(selectors.buttonBranchOverview).click();
+    }, 400);
 }
