74ad2f5fa69ccad7abfdd11ccd5bd613f1abb18c
Patrice Matzke <p.matzke@avm.de>
2020-02-12 11:28:14 +0100

    checkbox listener

--- jazz/create-bitbucket-branch.js
+++ jazz/create-bitbucket-branch.js
@@ -1,36 +1,40 @@
 // ==UserScript==
 // @name       [Jazz] button: create bitbucket branch
 // @include    https://jazz.avm.de/ccm/web/projects/AVM-Entwicklung*
-// @version    1.0.1
+// @version    1.0.2
 // @author     https://github.com/VlkaFenryka
 // ==/UserScript==
 
 setTimeout(function() {
-  function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-  }
-
   let title = document
     .querySelector(".SummaryFieldInner")
     .textContent.trim()
     .replace(/\W+/g, "-");
   let issueID = document.querySelector("title").textContent.match(/\d+/g)[0];
-  let el = document.createElement("a");
-  let inputs = `<input type="checkbox" id="checkbox-html" value="html" checked>
-                <label for="checkbox-html">HTML</label>
-                <br>
-                <input type="checkbox" id="checkbox-node-modules" value="node_modules">
-                <label for="checkbox-node-modules">node_modules</label></div>`;
+  let html = `<a href="https://bitbucket.avm.de/plugins/servlet/create-branch?issueid=${issueID}&title=-${encodeURI(
+    title
+  )}&html=true&node_modules=false" class="tab js-bitbucket-branch-create-button" target="_blank">create bitbucket branch</a>
+<input type="checkbox" id="checkbox-html" class="js-checkbox-html" checked>
+<label for="checkbox-html">HTML</label>
+<br>
+<input type="checkbox" id="checkbox-node-modules" class="js-checkbox-node-modules">
+<label for="checkbox-node-modules">node_modules</label></div>`;
 
-  el.href =
-    "https://bitbucket.avm.de/plugins/servlet/create-branch?issueid=" +
-    issueID +
-    "&title=-" +
-    encodeURI(title);
-  el.classList.add("tab");
-  el.target = "_blank";
-  el.innerHTML = "create bitbucket branch";
+  document.querySelector(".TabArea").insertAdjacentHTML("beforeend", html);
 
-  insertAfter(document.querySelector(".TabArea a:last-of-type"), el);
-  document.querySelector(".TabArea").insertAdjacentHTML("beforeend", inputs);
-}, 1000);
+  document
+    .querySelector(".js-bitbucket-branch-create-button")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      this.href = this.href.replace(
+        /html=(\w+)/gm,
+        `html=${document.querySelector(".js-checkbox-html").checked}`
+      );
+      this.href = this.href.replace(
+        /node_modules=(\w+)/gm,
+        `node_modules=${
+          document.querySelector(".js-checkbox-node-modules").checked
+        }`
+      );
+    });
+}, 2000);
