73203d7cbb297aed6d786ddad0d93b1d60bd1a51
Patrice Matzke <p.matzke@avm.de>
2020-02-12 11:51:45 +0100

    switch to radio button

--- jazz/create-bitbucket-branch.js
+++ jazz/create-bitbucket-branch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       [Jazz] button: create bitbucket branch
 // @include    https://jazz.avm.de/ccm/web/projects/AVM-Entwicklung*
-// @version    1.0.2
+// @version    1.0.3
 // @author     https://github.com/VlkaFenryka
 // ==/UserScript==
 
@@ -13,12 +13,14 @@ setTimeout(function() {
   let issueID = document.querySelector("title").textContent.match(/\d+/g)[0];
   let html = `<a href="https://bitbucket.avm.de/plugins/servlet/create-branch?issueid=${issueID}&title=-${encodeURI(
     title
-  )}&html=true&node_modules=false" class="tab js-bitbucket-branch-create-button" target="_blank">create bitbucket branch</a>
-<input type="checkbox" id="checkbox-html" class="js-checkbox-html" checked>
+  )}&repo=html" class="tab js-bitbucket-branch-create-button" target="_blank">create bitbucket branch</a>
+<fieldset>
+<input type="radio" id="checkbox-html" class="js-checkbox-html" name="repo" value="html" checked>
 <label for="checkbox-html">HTML</label>
 <br>
-<input type="checkbox" id="checkbox-node-modules" class="js-checkbox-node-modules">
-<label for="checkbox-node-modules">node_modules</label></div>`;
+<input type="radio" id="checkbox-node-modules" class="js-checkbox-node-modules" name="repo" value="node_modules">
+<label for="checkbox-node-modules">node_modules</label>
+</fieldset>`;
 
   document.querySelector(".TabArea").insertAdjacentHTML("beforeend", html);
 
@@ -26,15 +28,16 @@ setTimeout(function() {
     .querySelector(".js-bitbucket-branch-create-button")
     .addEventListener("click", function(event) {
       event.preventDefault();
-      this.href = this.href.replace(
-        /html=(\w+)/gm,
-        `html=${document.querySelector(".js-checkbox-html").checked}`
-      );
-      this.href = this.href.replace(
-        /node_modules=(\w+)/gm,
-        `node_modules=${
-          document.querySelector(".js-checkbox-node-modules").checked
-        }`
-      );
+      let repoValue = "";
+      switch (true) {
+        case document.querySelector(".js-checkbox-html").checked:
+          repoValue = "html";
+          break;
+        case document.querySelector(".js-checkbox-node-modules").checked:
+          repoValue = "node_modules";
+          break;
+      }
+
+      this.href = this.href.replace(/repo=(\w+)/gm, `repo=${repoValue}`);
     });
 }, 2000);
